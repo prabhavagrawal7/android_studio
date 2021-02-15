9be9304871221c0999d72a660fb5022a3a2c31f3
Prabhav Agrawal <prabhavagrawal7@gmail.com>
2021-02-16 00:46:38 +0530

    1

---
+++ Imagescroller/app/src/main/java/com/example/imagescroller/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.imagescroller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    public void sendNow(View view) {
+        Toast.makeText(this, "This is sending data from app...", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
