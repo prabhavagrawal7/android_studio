3a20deb06867892e5f51a72fa026aa45f394ed1b
Prabhav Agrawal <prabhavagrawal7@gmail.com>
2021-02-16 16:10:21 +0530

    1

--- Imagescroller/app/src/main/java/com/example/imagescroller/MainActivity.java
+++ Imagescroller/app/src/main/java/com/example/imagescroller/MainActivity.java
@@ -4,11 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
     public void sendNow(View view) {
+
+
         Toast.makeText(this, "This is sending data from app...", Toast.LENGTH_SHORT).show();
+
+
+    }
+
+    public void receiveNow(View view) {
+        Toast.makeText(this, "This is receiving data from app...", Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void deleteNow(View view) {
+        Toast.makeText(this, "This is deleting data from app...", Toast.LENGTH_SHORT).show();
+
     }
 
     @Override
